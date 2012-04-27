97bfe596ca9c3ed1cc9d1345c20d6cc08a374938
bjost2s <beate.jost@smail.inf.h-brs.de>
2012-04-27 09:51:30 +0200

    moved backgroundMap.png to bin/recources, to make this always avalable, adjusted Map.java for this

--- RefereePkg/src/model/Map.java
+++ RefereePkg/src/model/Map.java
@@ -3,6 +3,7 @@ package model;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -12,30 +13,34 @@ import view.Utils;
 public class Map {
 	private static final String imageFormat = new String("png");
 	private static BufferedImage map;
-	private static String mapFullName;
+	private static URL url;
 
 	public static BufferedImage loadBackgroundMap() {
-		 mapFullName = System.getProperty("user.home") + File.separator
-							+ "backgroundMap.png";		
-		File file = new File(mapFullName);
+		System.out.println(Class.class
+				.getResource("/recources/backgroundMap.png"));
+		url = Class.class.getResource("/recources/backgroundMap.png");
+		;
 		try {
-		map = ImageIO.read(file);
+			map = ImageIO.read(url);
 			return map;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	public static boolean saveTaskSpecMap(File file, MapArea mapArea) {
 
 		file = Utils.correctFile(file);
 		try {
-			BufferedImage image = new BufferedImage(mapArea.getMapPane().getWidth(),
-					mapArea.getMapPane().getHeight(), BufferedImage.TYPE_INT_RGB);
+			BufferedImage image = new BufferedImage(mapArea.getMapPane()
+					.getWidth(), mapArea.getMapPane().getHeight(),
+					BufferedImage.TYPE_INT_RGB);
 			mapArea.getMapPane().paintAll(image.getGraphics());
-			ImageIO.write(image, imageFormat, new File(file.getPath().substring(0, file.getPath().length()-4) + "." + imageFormat));
+			ImageIO.write(image, imageFormat, new File(file.getPath()
+					.substring(0, file.getPath().length() - 4)
+					+ "."
+					+ imageFormat));
 		} catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
 			return false;
