927bdb0d6f0e3101d13584fc9a1ddffe6c6250a4
Marc Wollenweber <marcwollenweber@web.de>
2012-06-05 20:10:54 +0200

    Add socket.close to cpp client

--- RobotPkg/src/cpp/cpp_client.h
+++ RobotPkg/src/cpp/cpp_client.h
@@ -12,24 +12,26 @@ std::string obtainTaskSpecFromServer(std::string server_ip, std::string server_p
 	zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REQ);
     
-    std::cout << "Connecting to the Referee…" << std::endl;
+    std::cout << "Connecting to server..." << std::endl;
     socket.connect (conn_infos.c_str());
     
     zmq::message_t* send_team_name = new zmq::message_t(team_name.size());
 	memcpy ((void *) send_team_name->data (), team_name.c_str(), team_name.size());
 	socket.send (*send_team_name);
+	std::cout << "Sent team name to server... " << std::endl;
 	
 	zmq::message_t reply(REPLYSTRINGLENGHT);
 	socket.recv (&reply);
 	std::string returnstring((char*)reply.data());
 	std::cout << returnstring << std::endl;
-	std::cout << "Received Triplet " << std::endl;
+	std::cout << "Recevied taskSpecification: " << returnstring << std::endl;
 	
 	std::string ack_msg = "ACK";
 	zmq::message_t* send_ack = new zmq::message_t(ack_msg.size());
 	memcpy ((void *) send_ack->data (), ack_msg.c_str(), ack_msg.size());
 	socket.send (*send_ack);
 	
+	socket.close();	
 
 	return returnstring;
 }
